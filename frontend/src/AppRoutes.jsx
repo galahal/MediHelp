f27ce9bd5ccb9249eb4832a16ec2acbec389c4d5
Sumaitha <sumaitha.jannat@g.bracu.ac.bd>
2024-12-07 18:55:41 +0600

    Dashboard

--- frontend/src/AppRoutes.jsx
+++ frontend/src/AppRoutes.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/Home/HomePage'; // Import the HomePage component
-
+import Dashboard from './pages/Dashboard/Dashboard';
 export default function AppRoutes() {
   return (
     <Routes> 
       <Route path="/" element={<HomePage />} /> {/* Render HomePage here */}
+      <Route
+    path="/dashboard"
+    element={<AuthRoute> <Dashboard /></AuthRoute>}/>
     </Routes>
+    
   );
 }
