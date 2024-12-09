7f0bb56009cb18081a43336c7a9e20f8e748446d
Shreyaa-Biswas <shreyabiswas838@gmail.com>
2024-12-09 20:30:19 +0600

    Updated pharmacy.model.js
    Add a reference to the User Model in the pharmacist field using Mongoose's ObjectId.

--- backend/src/models/pharmacy.model.js
+++ backend/src/models/pharmacy.model.js
@@ -1,10 +1,17 @@
-import { model, Schema } from 'mongoose';
+import { model, Schema, Types } from 'mongoose';
 
 export const PharmacySchema = new Schema(
   {
     name: { type: String, required: true },
     address: { type: String, required: true },
+    contact: { type: String, required: true },
     isEmergency: { type: Boolean, default: false },
+    // Reference to User Model
+    pharmacist: { 
+      type: Types.ObjectId, 
+      ref: 'user', // Matches the name of the User Model when Pharmacist is set to True
+      required: true 
+    },
   },
   {
     timestamps: true,
