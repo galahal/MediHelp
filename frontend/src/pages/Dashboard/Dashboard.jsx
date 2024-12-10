23752e5857eed3c5b54b33540946e7a10a914a31
Sumaitha <sumaitha.jannat@g.bracu.ac.bd>
2024-12-10 22:43:56 +0600

    Dashboard part 2

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from "./dashboard.module.css";
 
 export default function Dashboard() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const user = {
     name: "Faiyaz",
     role: "Admin", // Role can be "Admin", "Pharmacist", or "User"
     photo: "https://static.vecteezy.com/system/resources/previews/019/879/186/non_2x/user-icon-on-transparent-background-free-png.png", // URL to user's photo
   };
+  const toggleDropdown = () => {
+    setIsDropdownOpen((prev) => !prev);
+  };
   return (
     <div className={classes.dashboard}>
+      {/* Dropdown menu */}
+      <div className={classes.dropdown}>
+        <button onClick={toggleDropdown} className={classes.dropdownButton}>
+          &#9776; {/* 3 horizontal lines */}
+        </button>
+        {isDropdownOpen && (
+          <div className={classes.dropdownMenu}>
+            <ul className={classes.dropdownList}>
+              <li className={classes.dropdownItem}>Order History</li>
+              <li className={classes.dropdownItem}>Update Address</li>
+              <li className={classes.dropdownItem}>Update Password</li>
+              <li className={classes.dropdownItem}>Update Email</li>
+              <li className={classes.dropdownItem}>Update Phone Number</li>
+            </ul>
+          </div>
+        )}
+      </div>
+    {/* <div className={classes.dashboard}> */}
       <div className={classes.userInfo}>
         {/* User Photo */}
         <img
